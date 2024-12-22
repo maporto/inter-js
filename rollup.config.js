9246df43892946be6d74e64d70c64e68dc227b36
Marcos Porto <marcosbporto@gmail.com>
2024-12-22 06:25:57 -0300

    fix: make a build to publish

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import terser from '@rollup/plugin-terser'
+import typescript from '@rollup/plugin-typescript'
+import dts from 'rollup-plugin-dts'
+
+export default [
+  {
+    input: 'src/index.ts',
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: false,
+        declarationDir: null
+      }),
+      terser()
+    ],
+    output: [
+      {
+        file: 'dist/index.mjs',
+        format: 'esm'
+      },
+      {
+        file: 'dist/index.cjs',
+        format: 'cjs',
+        exports: 'named'
+      }
+    ]
+  },
+  {
+    input: 'src/index.ts',
+    plugins: [dts()],
+    output: {
+      file: 'dist/index.d.ts',
+      format: 'es'
+    }
+  }
+]
